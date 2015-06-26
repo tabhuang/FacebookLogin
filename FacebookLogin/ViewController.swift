8a2983f61758419660552ba5c1c049ea81f51ad0
Huang Jian-Yu <bubu19861213@gmail.com>
2015-06-26 15:27:11 +0800

    Initial Commit

---
+++ FacebookLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookLogin
+//
+//  Created by Huang Jian-Yu on 2015/6/26.
+//  Copyright (c) 2015年 Zitra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
